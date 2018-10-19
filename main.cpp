89c90a70a0c7cb8dfa5f9d52c839c164b40b870d
Teddy Ye <teddyzczy@126.com>
2018-10-19 10:40:33 +0800

    add testPointer

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 #include "string.h"
 using namespace std;
 void testCString();
-
+void testPoint();
 int main() {
     std::cout << "Hello, World!" << std::endl;
     testCString();
-    char cstr[] = {'C','+','+'};
-    cout << strlen(cstr) << endl;
+    testPoint();    
     return 0;
 }
-
+void testPoint(){
+	int a(3);
+	int *pa; 
+	cout << "a=" << a << endl;
+	cout << "pa="<<pa << endl;
+}
 void testCString(){
     char cstr[] = {'C','+','+'};
     cout << strlen(cstr) << endl;
